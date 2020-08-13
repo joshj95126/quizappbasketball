fe792104a224a5e296174cf043369b1140172b07
Joshua Joyce <66971622+joshj95126@users.noreply.github.com>
2020-08-13 10:41:56 -0700

    8/13
    Added more questions, gifs, fixed header image

--- index.js
+++ index.js
@@ -13,9 +13,44 @@ const state = {
             correct: 0
         },
         {
-            title: "Who is the NBA All Time leading scorer",
+            title: "Who is the NBA All Time leading scorer?",
             answers: ["Karl Malone", "Michael Jordan", "Kareem Abdul-Jabbar", "Lebron James"],
             correct: 2
+        },
+        {
+            title: "How many 3 point field goals did Shaquille O'Neal make in his career?",
+            answers: ["0", "1", "2", "6"],
+            correct: 1
+        },
+        {
+            title: "Who is the shortest player to ever play in the NBA?",
+            answers: ["Spud Webb", "Earl Boykins", "Keith Jennings", "Muggsy Bogues"],
+            correct: 3
+        },
+        {
+            title: "Which NBA player had his number retired by the Harlem Globetrotters?",
+            answers: ["Willie Gardner", "Wilt Chamberlain", "Connie Hawkins", "Nat Clifton"],
+            correct: 1
+        },
+        {
+            title: "What season was the 3 point shot implemented?",
+            answers: ["79-80", "80-81", "71-72", "59-60"],
+            correct: 0
+        },
+        {
+            title: "Scott Skiles holds the NBA record for how many assists in a game?",
+            answers: ["28", "25", "30", "18"],
+            correct: 2
+        },
+        {
+            title: "Which country won a gold medal at the 2004 Olympics in Greece?",
+            answers: ["Italy", "Serbia", "USA", "Argentina"],
+            correct: 3
+        },
+        {
+            title: "Which NBA player holds the record for most steals in an NBA playoff game (10)?",
+            answers: ["John Stockton", "Allen Iverson", "Jason Kidd", "Gary Payton"],
+            correct: 1
         }
     ]
 
@@ -24,7 +59,7 @@ const state = {
 $('body').on('submit', 'form', function(event) {
     event.preventDefault()
     const answer = event.target.answer.value
-    const question = state.questions[state.currentQuestion]
+
 
     renderFeedback(answer)
 
@@ -41,6 +76,7 @@ $('body').on('click', '#nextQuestion', function(event) {
             <button id = "nextQuestion">
             Play again
         </button>
+        <img src="crossover.gif"/>
         </section>
         `)
         state.currentQuestion = 0
@@ -88,7 +124,7 @@ function makeScore() {
 }
 
 function renderFeedback(answer) {
-
+    const question = state.questions[state.currentQuestion]
     if (answer == question.correct) {
         state.score++
             $('main').html(`<section id="answer">
@@ -124,6 +160,7 @@ function start() {
             <button id="nextQuestion">
             Click To Start
         </button>
+        <img src="curry.gif"/>
         </section>
     `)
 }
